4466905f877de8d09fa1c7545ae8079d566fced3
chengj <ab140140@163.com>
2019-08-23 09:12:11 +0800

    init files

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,23 @@ import React from 'react';
 import {Button} from 'antd'
 import './App.css';
 
+const obj:Object = {
+	"username":"jack",
+	"password":"123"
+},
+apiUrl:string = '/blog/login';
+
+fetch(apiUrl, {
+  method: 'post',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify(obj)
+}).then((res) => {
+  if (res.status !== 200) {
+      throw new Error('错误 ' + res);
+  }
+  return res.json().then((resJson) => resJson )
+})
+
 const App: React.FC = () => {
   return (
     <div className="App">
@@ -11,5 +28,7 @@ const App: React.FC = () => {
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/views/login/views/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface LoginProps {
+
+}
+
+interface LoginState{
+
+}
+
+export default class Login extends React.Component<LoginProps,LoginState>{
+    
+}
