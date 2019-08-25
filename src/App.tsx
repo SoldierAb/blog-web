55fb2b66194c140ce52c118e6efd53b918eac362
SoldierAb <ab140140@163.com>
2019-08-25 21:40:36 +0800

    feat: add fetchUtil

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import {Button} from 'antd'
 import './App.css';
+import {fetchGet ,fetchPost} from './utils/fetchUtil'
 
 const obj:Object = {
 	"username":"jack",
 	"password":"123"
 },
-apiUrl:string = '/blog/login';
+apiUrl:string = '/blog/login?name={username}';
+fetchPost(apiUrl,obj)
 
-fetch(apiUrl, {
-  method: 'post',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify(obj)
-}).then((res) => {
-  if (res.status !== 200) {
-      throw new Error('错误 ' + res);
-  }
-  return res.json().then((resJson) => resJson )
-})
+
+// fetch(apiUrl, {
+//   method: 'post',
+//   headers: { 'Content-Type': 'application/json' },
+//   body: JSON.stringify(obj)
+// }).then((res) => {
+//   if (res.status !== 200) {
+//       throw new Error('错误 ' + res);
+//   }
+//   return res.json().then((resJson) => resJson )
+// })
 
 const App: React.FC = () => {
   return (
